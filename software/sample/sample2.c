42c98d48c831cdd9ab5f026e04b8f709d2dd62d0
miiiika0831 <agumikastd831@gmail.com>
2024-07-04 18:06:28 +0900

    不快指数に関して

--- software/sample/sample2.c
+++ software/sample/sample2.c
@@ -7,10 +7,14 @@ int main() {
     srand(time(0));
 
     // 0から100までの乱数を生成する
-    int random_number = rand() % 101;
+    int temperature = rand() % 101;
+    int humidity = rand() % 101;
+
+    double ans;
+    ans = 0.81*temperature+0.01*humidity*(0.99*temperature-14.3)+46.3;
 
     // 生成した乱数を出力する
-    printf("%d\n", random_number);
+    printf("%d\n", ans);
 
     return 0;
 }
